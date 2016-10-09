55d8cd6682056a3e80c38893231e07f83e2bc0ea
Adam Buechler <butchler@gmail.com>
2016-10-09 23:45:49 +0900

    Set up eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,31 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "settings": {
+    "import/resolver": {
+      "webpack": {
+        "config": "webpack.config.js"
+      }
+    }
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true,
+      "experimentalObjectRestSpread": true,
+    },
+  },
+  "rules": {
+    "strict": 0,
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    'no-use-before-define': 0,
+    'no-else-return': 0,
+    'no-use-before-define': 0,
+  },
 };
