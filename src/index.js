3ea33f26dc5d26f753f1c4870f4eb59f694df9a6
Adam Buechler <butchler@gmail.com>
2016-11-10 02:19:25 +0900

    Experiment with alternative state management style.

--- src/index.js
+++ src/index.js
@@ -32,3 +32,126 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('app-container')
 );
+
+const reducers = new ReducerSet();
+export default reducers;
+
+import reducers from 'globals/reducers';
+import subscribe from 'helpers/subscribe';
+const actionList = reducers.add(
+  ['actionType1', 'actionType2'],
+  (state = [], action) => state.push(action)
+);
+subscribe(actionList)(Component);
+
+import reducers from 'globals/reducers';
+import subscribe from 'helpers/subscribe';
+subscribe(reducers.add(
+  ['actionType1', 'actionType2'],
+  (state = [], action) => state.push(action)
+))(Component);
+
+import reducers from 'globals/reducers';
+
+function actionList(state = [], action) {
+  if (action.type === 'actionType1' || action.type === 'actionType2') {
+    return state.push(action);
+  }
+}
+
+actionList.actionTypes = ['actionType1', 'actionType2'];
+
+import reducers from 'globals/reducers';
+
+const append = (state, action) => state.push(action);
+reducers.add({
+  initialState: [],
+  handlers: {
+    [action1]: append,
+    [action2]: append,
+  },
+});
+
+// components/component.js
+import { subscribe } from 'globals/store';
+import ComponentView from './ComponentView';
+
+const appendAction = (state, action) => state.push(action);
+
+export default subscribe({
+  initialState: [],
+  handlers: {
+    [action1]: appendAction,
+    [action2]: appendAction,
+  },
+})(ComponentView);
+
+
+
+
+
+
+
+
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'globals/store';
+import Provider from 'helpers/Provider';
+import Component from 'components/component';
+import { init } from 'constants/actions';
+
+const store = createStore();
+
+store.dispatch(init());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Component />
+  </Provider>
+);
+
+// globals/subscriptions.js
+import Store from 'helpers/Store';
+
+export default new Store();
+
+// components/component.js
+import { subscribe } from 'globals/store';
+import createReducer from 'helpers/createReducer';
+import ComponentView from './ComponentView';
+import { action1, action2, myAction, globalAction } from 'constants/actions';
+
+const appendAction = (state, action) => state.push(action);
+
+export const reducer = createReducer([], {
+  [action1]: appendAction,
+  [action2]: appendAction,
+});
+
+subscribe(reducer, {
+  myAction: (payload, props) => myAction({ payload, id: props.id }),
+  globalAction,
+}, (state, props) => state[props.id])(ComponentView);
+
+// helpers/Store.js
+export default class Store {
+  subscribe(reducer, actionCreators, mapStateAndPropsToProps) {
+    // TODO
+  }
+
+  createStore() {
+    // TODO
+  }
+}
+
+// helpers/Provider.js
+import React, { Component, PropTypes } from 'react';
+
+export default class Provider extends Component {
+}
+
+// helpers/createReducer.js
+export default function createReducer(initialState, actionHandlers) {
+  // TODO
+}
