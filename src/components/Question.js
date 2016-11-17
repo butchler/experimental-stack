2dbad647cb718551a12060887b5a5691ab9ee34f
Adam Buechler <butchler@gmail.com>
2016-11-18 08:33:40 +0900

    WIP on immutable mutator idea.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,5 @@
 import { Record, List, Map } from 'immutable';
 import { Reducer } from 'globals/store';
-import { set, setDefault, update, remove } from 'helpers/mutators';
 import subscribe from 'helpers/subscribe';
 import {
   removeQuiz, removeQuestion, addAnswer, removeAnswer, setQuestionText,
@@ -12,36 +11,22 @@ const Question = Record({ text: '', answerIds: List() });
 
 export const questionsReducer = Reducer(State(), [
   [removeQuiz, (state, id) => state.removeIn(['quizzes', id])],
-  [removeQuiz, (state, id) => remove(['quizzes', id])],
   [removeQuestion, (state, { quizId, questionId }) => state.removeIn(['quizzes', quizId, questionId])],
-  [removeQuestion, (state, { quizId, questionId }) => remove(['quizzes', quizId, questionId])],
   [addAnswer, (state, { quizId, questionId, answerId }) => state.updateIn(
     ['quizzes', quizId, questionId],
     Question(),
     question => question.update('answerIds', ids => ids.push(answerId))
   )],
-  [addAnswer, (state, { quizId, questionId, answerId }) => [
-    setDefault(['quizzes', quizId, questionId], Question()),
-    update(['quizzes', quizId, questionId, 'answerIds'], ids => ids.push(answerId)),
-  ]],
   [removeAnswer, (state, { quizId, questionId, answerId }) => state.updateIn(
     ['quizzes', quizId, questionId, 'answerIds'],
     List(),
     ids => ids.filter(id => id !== answerId)
   )],
-  [removeAnswer, (state, { quizId, questionId, answerId }) =>
-     state.getIn(['quizzes', quizId, questionId]) &&
-       update(['quizzes', quizId, questionId, 'answerIds'], ids =>
-         ids.filter(id => id !== answerId))],
   [setQuestionText, (state, { quizId, questionId, text }) => state.updateIn(
     ['quizzes', quizId, questionId],
     Question(),
     question => question.set('text', text)
   )],
-  [setQuestionText, (state, { quizId, questionId, text }) => [
-    setDefault(['quizzes', quizId, questionId], Question()),
-    set(['quizzes', quizId, questionId, 'text'], text),
-  )]],
 ]);
 
 export default subscribe(
@@ -57,3 +42,37 @@ export default subscribe(
   ({ quizzes }, { quizId, questionId }) =>
     quizzes.getIn([quizId, questionId], Question())
 )(QuestionView);
+
+/*
+import { set, setIfNot, update, remove } from 'helpers/mutators';
+
+export const questionsReducer = Reducer(State(), [
+  [removeQuiz, (state, id) => remove(['quizzes', id])],
+  [removeQuestion, (state, { quizId, questionId }) => remove(['quizzes', quizId, questionId])],
+  [addAnswer, (state, { quizId, questionId, answerId }) => [
+    setIfNot(['quizzes', quizId, questionId], Question()),
+    update(['quizzes', quizId, questionId, 'answerIds'], ids => ids.push(answerId)),
+  ]],
+  [removeAnswer, (state, { quizId, questionId, answerId }) =>
+     state.getIn(['quizzes', quizId, questionId]) &&
+       update(['quizzes', quizId, questionId, 'answerIds'], ids =>
+         ids.filter(id => id !== answerId))],
+  [setQuestionText, (state, { quizId, questionId, text }) => [
+    setIfNot(['quizzes', quizId, questionId], Question()),
+    set(['quizzes', quizId, questionId, 'text'], text),
+  ]],
+]);
+
+export default subscribe(
+  questionsReducer,
+  {
+    addAnswer,
+    removeAnswer: ({ quizId, questionId }, answerId) =>
+      removeAnswer({ quizId, questionId, answerId }),
+    setQuestionText: ({ quizId, questionId }, text) =>
+      setQuestionText({ quizId, questionId, text }),
+    removeQuestion,
+  },
+  ({ quizId, questionId }) => ['quizzes', quizId, questionId],
+)(QuestionView);
+*/
